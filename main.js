a82c7a0d4d10430b5cd4e7c2a38522763ccb558a
Tyler Collingridge <tylercollingridge@gmail.com>
2024-06-11 14:16:54 -0600

    Create changeDisplay function
    - Displays the text content passed when clicking
    a button.

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-// create onclick event for all buttons
+function changeDisplay(displayContent) {
+    const display = document.querySelector(".display"); 
+
+    display.textContent += displayContent; 
+}
+
 function createOnClickEvents() {
     const buttons = Array.from(document.querySelectorAll("button")); 
 
-    buttons.forEach((button) => button.addEventListener("click", (event) => {
-        console.log(event.textContent); 
+    buttons.forEach((button) => button.addEventListener("click", () => {
+        changeDisplay(button.textContent); 
     })); 
 }
 
