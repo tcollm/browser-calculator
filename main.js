050622878d3facd60dd8dcc4f3876f58ee7e54f0
Tyler Collingridge <tylercollingridge@gmail.com>
2024-06-11 14:24:24 -0600

    Create emphasizeButton function to set the pressed button's border to black for 300ms

--- main.js
+++ main.js
@@ -3,17 +3,32 @@ function changeDisplay(displayContent) {
     if (display.textContent === "0") {
         display.textContent = ""; 
     }
-    
+
     display.textContent += displayContent; 
+    // checkValidInput(currentDisplayContent, contentToAdd); 
 }
 
 function createOnClickEvents() {
     const buttons = Array.from(document.querySelectorAll("button")); 
 
     buttons.forEach((button) => button.addEventListener("click", () => {
+        emphasizeButton(button); 
         changeDisplay(button.textContent); 
     })); 
 }
+
+function emphasizeButton(button) {
+    button.style.border = "2px solid black"; 
+
+
+    setTimeout(() => {
+        button.style.border = "none"; 
+    }, 300); 
+}
+
+// checks if the next input is valid. Ex: cannot put "+" after "="
+// function checkValidInput() {
+// }
 
 
 function main() {
